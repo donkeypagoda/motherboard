bfee3006675497eb5f9e81da70a79f1b561b6a35
donkeypagoda <christopherallenstewart@gmail.com>
2017-10-09 10:45:05 -0700

    basic server is up and running

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const path = require("path");
 const cookie = require("cookie-parser");
 const morgan = require("morgan");
 
+// if (process.env.NODE_ENV !== 'production') {
+//   require('dotenv').config();
+// }
+
+const port = process.env.PORT || 5000;
+
 const app = express();
 app.use(bodyParser.json());
 app.use(cookie());
