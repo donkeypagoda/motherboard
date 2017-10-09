0173b7c7ed4f764d2314685d8149f0fa4f80d64a
donkeypagoda <christopherallenstewart@gmail.com>
2017-10-09 09:42:58 -0700

    installing dependancies and starting the server file

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parse");
 const path = require("path");
-const cookie = require("cookie");
+const cookie = require("cookie-parser");
+const morgan = require("morgan");
 
 const app = express();
+app.use(bodyParser.json());
+app.use(cookie());
+app.use(morgan('dev'))
