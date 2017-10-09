294d4d113ee2f31d81cdaffbbec0e32d8457f3e6
donkeypagoda <christopherallenstewart@gmail.com>
2017-10-09 13:59:13 -0700

    impatient raja

---
+++ public/js/app.routes.js
@@ -0,0 +1,20 @@
+(function() {
+  'use strict';
+
+  angular.module('app').config(config)
+
+  config.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider']
+
+  function config($stateProvider, $urlRouterProvider, $locationProvider){
+
+    $locationProvider.html5Mode(true)
+
+    $stateProvider
+      .state({
+        name: 'test',
+        url: '/',
+        directive: 'test'
+      })
+
+  }
+})();
