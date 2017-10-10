c160fe9adabc342dc4561b10818e2916d099bd68
donkeypagoda <christopherallenstewart@gmail.com>
2017-10-10 16:05:54 -0700

    fuckity fuck fuck fuck

--- migrations/20171010151527_users.js
+++ migrations/20171010151527_users.js
@@ -1,8 +1,15 @@
+'use strict';
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('users', (table) => {
+    table.increments();
+    table.string('username').notNullable().unique();
+    table.string('hashed_password').notNullable();
+    table.string('email').notNullable().unique();
+    table.json('saved_boards').defaultTo([]);
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('users')
 };
