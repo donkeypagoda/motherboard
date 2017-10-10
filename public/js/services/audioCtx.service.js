a6aeacbc396bafd2bba0ebfb17c68225b5f72cb1
donkeypagoda <christopherallenstewart@gmail.com>
2017-10-10 14:07:35 -0700

    I'm stuck on the audioservice and breakout, so I'm going to go back to working on kontrol

--- public/js/services/audioCtx.service.js
+++ public/js/services/audioCtx.service.js
@@ -1,14 +1,28 @@
 (function() {
   'use strict';
   angular.module('app')
-    .service('audioCtx', service)
+    .service('delayService', service)
 
     // service.$inject = ["$http", "$stateParams"]
 
     function service(){
+      class AsyncUnits{
+        constructor(audioCtx, unitArray){
+          this.units = unitArray;
+          this.length = unitArray.length;
+          this.count = 0;
+          this.invoke = () =>{
+            if (this.count < this.length){
+              this.units[this.count++](this.invoke.bind(this))
+            }
+          }
+        }
+      }
 
-      this.makeAudioAndDelay = function(){
+      this.buildSignalPath = function(unitArray){
         const audioCtx = new AudioContext();
+        const newChain = new AsyncUnits(audioCtx, unitArray);
+
         if (navigator.mediaDevices.getUserMedia) {
           console.log("yah buddy getUserMedia is down with the plan");
           return navigator.mediaDevices.getUserMedia({audio: { latency: 0.01,
@@ -20,11 +34,14 @@
             const source = audioCtx.createMediaStreamSource(stream);
             return source;
           })
+
+
+          newChain.invoke();
+
+
+
           .then((source) => {
-            return new Delay(audioCtx, source)
-          })
-          .then((delay) => {
-            return delay.output.connect(audioCtx.destination)
+            return source.output.connect(audioCtx.destination)
           })
           .catch(function(err) {
                 console.log('The following gUM error occured: ' + err);
