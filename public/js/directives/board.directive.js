cf5d8789945ea9d8e23aefb7f9e06f56dbdc4de9
donkeypagoda <christopherallenstewart@gmail.com>
2017-10-10 16:09:45 -0700

    for raja

--- public/js/directives/board.directive.js
+++ public/js/directives/board.directive.js
@@ -4,22 +4,22 @@
     .directive('board', function($compile) {
       const controller = function($scope, delayService) {
         const vm = this
-        // vm.$onInit = function (){
-        //   delayService.makeAudioAndDelay()
-        //     .then((delay) => {
-        //       console.log(delay);
-        //     })
-        // }
-        $scope.content = []
-        // this.addDelay = function(){
-        //   $scope.content.push("<delay></delay>")
-        // }
-        // this.addDelay();
-        // delay.makeAudioAndDelay();
-        this.addDisto = function (){
-          $scope.content.push("<disto></disto>")
+        vm.$onInit = function (){
+          delayService.makeAudioAndDelay()
+            .then((delay) => {
+              console.log(delay);
+            })
         }
-        this.addDisto();
+        $scope.content = []
+        this.addDelay = function(){
+          $scope.content.push("<delay></delay>")
+        }
+        this.addDelay();
+        // delay.makeAudioAndDelay();
+        // this.addDisto = function (){
+        //   $scope.content.push("<disto></disto>")
+        // }
+        // this.addDisto();
       }
       controller.$inject = ["$scope", "delayService"];
 
