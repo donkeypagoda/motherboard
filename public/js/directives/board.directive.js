312dc60070a19312005550423d225b59a7ff25aa
donkeypagoda <christopherallenstewart@gmail.com>
2017-10-14 14:50:05 -0700

    integrating RAJA stuff

--- public/js/directives/board.directive.js
+++ public/js/directives/board.directive.js
@@ -1,40 +1,6 @@
 (function() {
   'use strict';
   angular.module('app')
-<<<<<<< HEAD
-    .directive('board', function($compile) {
-      const controller = function($scope) {
-        const vm = this
-
-        $scope.content = []
-        vm.addUnit = function(unit){
-          // <disto></disto><chorus></chorus><delay></delay><panner></panner><reverb></reverb>
-          $scope.content.push("<reverb></reverb>") // ("<delay config="pedal data from database"></delay><delay></delay>")
-        }
-        vm.addUnit();
-      } // end of controller
-
-      controller.$inject = ["$scope"];
-
-      function link($scope, element, iElement) {
-        const root = $(iElement)
-        // const pedalSelect = root.find("#addPedalSelect")
-        // console.log(pedalSelect.val());
-        // pedalSelect.change(() => {
-        //
-        // })
-        element.html($scope.content.join(""));
-        $compile(element.contents())($scope);
-      }
-      link.$inject = ["$scope"]
-      // end of link
-
-      return {
-        controller: controller,
-        link: link
-        // templateUrl: "templates/board.template.html"
-      }
-=======
     .directive('board', function() {
       return {
         controller,
@@ -48,8 +14,7 @@
             <pedal-holder unit="{{u}}"></pedal-holder>
           </div>
           `
-      } // end of link
->>>>>>> dynamicPedals
+      }
     }) // end of directive
 
     controller.inject = ['unitService'];
