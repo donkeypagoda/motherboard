48e94a9399aa701a34bc904244c03c4208d6e819
donkeypagoda <christopherallenstewart@gmail.com>
2017-10-14 13:04:27 -0700

    stuck again, gosh darn and shucks, and dangit

--- public/js/directives/headMenu.directive.js
+++ public/js/directives/headMenu.directive.js
@@ -10,15 +10,23 @@
     }) // end of directive
     controller.$inject = []
     function controller(){
+      const vm = this
     }
-    link.$inject = ["$scope"]
+    link.$inject = ["$scope", "audioCtxService"]
     function link($scope, iElement, iAttrs, controller, transcludeFn){
       const root = $(iElement)
       // controller.pedalSelect = root.find("#addPedalSelect")
       // console.log(controller.pedalSelect);
-      const pedalSelect = root.find("#addPedalSelect")
-      pedalSelect.change(() => {
-        console.log(pedalSelect.val());
+      controller.pedalSelect = root.find("#addPedalSelect")
+      controller.pedalSelect.change(() => {
+        console.log(controller.pedalSelect.val());
+        $scope.content.push(controller.pedalSelect.val())
+        console.log($scope.content);
+        // element.html($scope.content.join(""));
+        // $compile(element.contents())($scope);
+
+          // <disto></disto><chorus></chorus><delay></delay><panner></panner><reverb></reverb>
+          // ("<delay config="pedal data from database"></delay><delay></delay>")
 
       })
     }
