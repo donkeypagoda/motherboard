97a50236787a908c25212663ce82b0176e97bccf
donkeypagoda <christopherallenstewart@gmail.com>
2017-10-14 13:32:03 -0700

    knobs in place on all units, and working.  definitely need so design work and css tweaking to make the pedals pretty

--- public/js/directives/reverb.directive.js
+++ public/js/directives/reverb.directive.js
@@ -87,21 +87,21 @@
         controller.getImpulseResponse.send();
       })
 
-      controller.reverbAmount.change(() => {
-        // console.log(parseFloat(controller.reverbAmount.val()));
-        controller.reverbInputGain.gain.value = parseFloat(controller.reverbAmount.val());
-      })
+      // controller.reverbAmount.change(() => {
+      //   // console.log(parseFloat(controller.reverbAmount.val()));
+      //   controller.reverbInputGain.gain.value = parseFloat(controller.reverbAmount.val());
+      // })
 
-      controller.reverbMix.change(() => {
-        // console.log(parseFloat(controller.reverbMix.val()));
-        controller.reverbOutputGain.gain.value = parseFloat(controller.reverbMix.val());
-        controller.cleanGain.gain.value = 0.1 / parseFloat(controller.reverbMix.val());
-      })
+      // controller.reverbMix.change(() => {
+      //   // console.log(parseFloat(controller.reverbMix.val()));
+      //   controller.reverbOutputGain.gain.value = parseFloat(controller.reverbMix.val());
+      //   controller.cleanGain.gain.value = 0.1 / parseFloat(controller.reverbMix.val());
+      // })
 
-      controller.reverbLoPass.change(() => {
-        // console.log(parseFloat(controller.reverbLoPass.val()));
-        controller.loPass.frequency.value = parseFloat(controller.reverbLoPass.val());
-      })
+      // controller.reverbLoPass.change(() => {
+      //   // console.log(parseFloat(controller.reverbLoPass.val()));
+      //   controller.loPass.frequency.value = parseFloat(controller.reverbLoPass.val());
+      // })
 
       controller.reverbHiPass.change(() => {
         // console.log(parseFloat(controller.reverbHiPass.val()));
@@ -128,6 +128,78 @@
           controller.reverbOutputGain.gain.value = parseFloat(controller.reverbMix.val())
         }
       })
+      controller.reverbAmount.knob({
+        fgColor:"#222222",
+        bgColor:"#EEEEEE",
+        thickness: 0.3,
+        angleOffset: -130,
+        angleArc: 260,
+        min: 0.0,
+        step: 0.1,
+        max: 3.5,
+        width: 100,
+        height: 100,
+        change : function (value) {
+          console.log("reverb amount : ", value);
+          controller.reverbInputGain.gain.value = parseFloat(controller.reverbAmount.val());
+        }
+      })
+      .css({display:'inline',padding:'0px 10px'});
+
+      controller.reverbMix.knob({
+        fgColor:"#222222",
+        bgColor:"#EEEEEE",
+        thickness: 0.3,
+        angleOffset: -130,
+        angleArc: 260,
+        min: 0.0,
+        step: 0.01,
+        max: 0.9,
+        width: 100,
+        height: 100,
+        change : function (value) {
+          console.log("reverb mix : ", value);
+          controller.reverbOutputGain.gain.value = parseFloat(controller.reverbMix.val());
+          controller.cleanGain.gain.value = 0.1 / parseFloat(controller.reverbMix.val());
+        }
+      })
+      .css({display:'inline',padding:'0px 10px'});
+
+      controller.reverbLoPass.knob({
+        fgColor:"#222222",
+        bgColor:"#EEEEEE",
+        thickness: 0.3,
+        angleOffset: -130,
+        angleArc: 260,
+        min: 1000,
+        step: 50,
+        max: 15000,
+        width: 100,
+        height: 100,
+        change : function (value) {
+          console.log("reverb Lo Pass : ", value);
+          controller.loPass.frequency.value = parseFloat(controller.reverbLoPass.val());
+        }
+      })
+      .css({display:'inline',padding:'0px 10px'});
+
+      controller.reverbHiPass.knob({
+        fgColor:"#222222",
+        bgColor:"#EEEEEE",
+        thickness: 0.3,
+        angleOffset: -130,
+        angleArc: 260,
+        min: 1,
+        step: 20,
+        max: 5000,
+        width: 100,
+        height: 100,
+        change : function (value) {
+          console.log("reverb Hi Pass : ", value);
+          controller.hiPass.frequency.value = parseFloat(controller.reverbHiPass.val());
+        }
+      })
+      .css({display:'inline',padding:'0px 10px'});
 
     } //end of link
 
