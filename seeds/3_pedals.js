7012aaab364f78690ca03e0703d496a5e1df6680
donkeypagoda <christopherallenstewart@gmail.com>
2017-10-16 12:12:24 -0700

    changed my seeds and migrations

--- seeds/3_pedals.js
+++ seeds/3_pedals.js
@@ -2,28 +2,28 @@ const pedals = [
   {
     id: 1,
     pedal_name: 'disto',
-    pedal_template: [150, 0.1, 60, 15000, true]
+    pedal_template: '[150, 0.1, 60, 15000, true]'
 
   },
   {
     id: 2,
     pedal_name: 'chorus',
-    pedal_template: [50, 300, true]
+    pedal_template: '[50, 300, true]'
   },
   {
     id: 3,
     pedal_name: 'delay',
-    pedal_template: [0.25, 0.0, 0.5, true, true, true]
+    pedal_template: '[0.25, 0.0, 0.5, true, true, true]'
   },
   {
     id: 4,
     pedal_name: 'reverb',
-    pedal_template: [0.7, 0.5, 125, 13000, true, true, "plate"]
+    pedal_template: '[0.7, 0.5, 125, 13000, true, true, "plate"]'
   },
   {
     id: 5,
     pedal_name: 'panner',
-    pedal_template: [125, true]
+    pedal_template: '[125, true]'
   }]
 
 exports.seed = function(knex, Promise) {
--- seeds/4_Pb.js
+++ seeds/4_Pb.js
@@ -3,35 +3,35 @@ const pbs = [
     id: 1,
     board_id: 1,
     pedal_id: 1,
-    pedal_config: JSON.stringify([150, 0.1, 60, 15000, true]),
+    pedal_config: '[150, 0.1, 60, 15000, true]',
     pedal_order: 1
   },
   {
     id: 2,
     board_id: 1,
     pedal_id: 3,
-    pedal_config: JSON.stringify([0.25, 0.0, 0.5, true, true, true]),
+    pedal_config: '[0.25, 0.0, 0.5, true, true, true]',
     pedal_order: 2
   },
   {
     id: 3,
     board_id: 2,
     pedal_id: 1,
-    pedal_config: JSON.stringify([150, 0.1, 60, 15000, true]),
+    pedal_config: '[150, 0.1, 60, 15000, true]',
     pedal_order: 1
   },
   {
     id: 4,
     board_id: 2,
     pedal_id: 2,
-    pedal_config: JSON.stringify([50, 300, true]),
+    pedal_config: '[50, 300, true]',
     pedal_order: 2
   },
   {
     id: 5,
     board_id: 2,
     pedal_id: 5,
-    pedal_config: JSON.stringify([125, true]),
+    pedal_config: '[125, true]',
     pedal_order: 3
   }]
 exports.seed = function(knex, Promise) {
